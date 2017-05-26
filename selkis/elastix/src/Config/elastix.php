9f290a90944a0db52b9e920aaf7f7f79e5ed7d94
Yessica Garcia <y3ssic454@gmail.com>
2017-05-26 18:08:42 -0400

    Add description config file

--- selkis/elastix/src/Config/elastix.php
+++ selkis/elastix/src/Config/elastix.php
@@ -5,107 +5,100 @@ return [
     | Host Elastix
     |--------------------------------------------------------------------------
     |
-    | This value determines the "environment" your application is currently
-    | running in. This may determine how you prefer to configure various
+    | This value determines the "host" the serve install Elastix.
+    | This may determine how you prefer to configure various
     | services your application utilizes. Set this in your ".env" file.
     |
     */
-	'host' => env('HOST_ELASTIX', '192.168.10.118'),
+	'host' => env('HOST_ELASTIX'),
 
     /*
     |--------------------------------------------------------------------------
-    | Host Elastix
+    | Port Elastix
     |--------------------------------------------------------------------------
     |
-    | This value determines the "environment" your application is currently
-    | running in. This may determine how you prefer to configure various
+    | This value determines the "port" the serve install Elastix.
+    | This may determine how you prefer to configure various
     | services your application utilizes. Set this in your ".env" file.
     |
     */
-    'port' => env('POST_ELASTIX', '5038'),
+    'port' => env('POST_ELASTIX'),
 
 	/*
     |--------------------------------------------------------------------------
     | User Elastix
     |--------------------------------------------------------------------------
     |
-    | This value determines the "environment" your application is currently
-    | running in. This may determine how you prefer to configure various
+    | This value determines the "user" the serve install Elastix.
+    | This may determine how you prefer to configure various
     | services your application utilizes. Set this in your ".env" file.
     |
     */
-	'user' => env('USER_ELASTIX', 'gareapi'),
+	'user' => env('USER_ELASTIX'),
+
+	/*
+    |--------------------------------------------------------------------------
+    | Password Elastix
+    |--------------------------------------------------------------------------
+    |
+    | This value determines the "password" the serve install Elastix.
+    | This may determine how you prefer to configure various
+    | services your application utilizes. Set this in your ".env" file.
+    |
+    */  
+	'password' => env('PASSWORD_ELASTIX'),
 
     /*
     |--------------------------------------------------------------------------
-    | User Elastix
+    | Model Elastix
     |--------------------------------------------------------------------------
     |
-    | This value determines the "environment" your application is currently
-    | running in. This may determine how you prefer to configure various
-    | services your application utilizes. Set this in your ".env" file.
+    | This value determines the model used to drive the assignment of extensions
+    | acoording to the ip of requesting machine. 
+    |
+    */
+    'ext_model' => App\Models\ipExt::class,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Call waiting time Elastix
+    |--------------------------------------------------------------------------
+    |
+    | This value determines the "waiting time" of dialing the call. Set this in your ".env" 
+    | file.
     |
     */
     'waitTime' => env('WAIT_TIME_ELASTIX', 45),
 
     /*
     |--------------------------------------------------------------------------
-    | User Elastix
+    | Line Elastix
     |--------------------------------------------------------------------------
     |
-    | This value determines the "environment" your application is currently
-    | running in. This may determine how you prefer to configure various
-    | services your application utilizes. Set this in your ".env" file.
+    | This value determines the "line" of elastix. Set this in your ".env" file.
     |
     */
     'troncal' => env('TRONCAL_ELASTIX', 'DAHDI/g11/'),
 
-	/*
-    |--------------------------------------------------------------------------
-    | password Elastix
-    |--------------------------------------------------------------------------
-    |
-    | This value determines the "environment" your application is currently
-    | running in. This may determine how you prefer to configure various
-    | services your application utilizes. Set this in your ".env" file.
-    |
-    */
-	'password' => env('PASSWORD_ELASTIX', 'gare2014.'),
-
     /*
     |--------------------------------------------------------------------------
-    | Passaword Elastix
+    | Value column input extension
     |--------------------------------------------------------------------------
     |
-    | This value determines the "environment" your application is currently
-    | running in. This may determine how you prefer to configure various
-    | services your application utilizes. Set this in your ".env" file.
-    |
-    */
-   
-    'ext_model' => App\Models\ipExt::class,
-
-    /*
-    |--------------------------------------------------------------------------
-    | Passaword Elastix
-    |--------------------------------------------------------------------------
-    |
-    | This value determines the "environment" your application is currently
-    | running in. This may determine how you prefer to configure various
-    | services your application utilizes. Set this in your ".env" file.
+    | This value determines the name of column in the table defined in attribute  
+    | "ext_model" used to defined the input extension according to the request ip.
     |
     */
    
     'value_ext_in' => 'ext_in',
 
-    /*
+     /*
     |--------------------------------------------------------------------------
-    | Passaword Elastix
+    | Value column output extension
     |--------------------------------------------------------------------------
     |
-    | This value determines the "environment" your application is currently
-    | running in. This may determine how you prefer to configure various
-    | services your application utilizes. Set this in your ".env" file.
+    | This value determines the name of column in the table defined in attribute
+    | "ext_model" used to defined the output extension according to the request ip.
     |
     */
    
